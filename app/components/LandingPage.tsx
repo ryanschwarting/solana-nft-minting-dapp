94c5c023bf9853cfbb9116eb290561a5a8a1d941
ryanschwarting <schwartingryan@yahoo.com>
2024-07-08 16:58:02 -0600

    hover effect on modal x button

--- app/components/LandingPage.tsx
+++ app/components/LandingPage.tsx
@@ -183,12 +183,14 @@ export const LandingPage = () => {
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 ">
           <div className="relative justify-center bg-black rple p-6 rounded-lg shadow-2xl shadow-sol-purple w-[800px]">
             <div className="flex flex-col justify-center items-center gap-2">
-              <button
+              <motion.button
+                whileHover={{ scale: 0.9 }}
+                whileTap={{ scale: 0.8 }}
                 className="absolute top-0 right-0 m-4 text-gray-500 hover:text-gray-700"
                 onClick={() => setShowModal(false)}
               >
                 <IoClose className="w-[30px] h-[30px] text-white" />
-              </button>
+              </motion.button>
               <h2 className="text-[24px] font-bold mb-4 text-white">
                 MINT SUCCESSFUL!{" "}
               </h2>
